f8dfaaa7d73a2bd36037bc03f8802be039b62808
AGX <agxnh2020@gmail.com>
2025-06-30 03:46:17 +0300

    initial Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/AGX18/goWorkoutAPI
+
+go 1.24.3
---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/AGX18/goWorkoutAPI/internal/app"
+)
+
+func main() {
+	var port int
+	flag.IntVar(&port, "port", 8080, "go backend server port")
+	flag.Parse()
+
+	app, err := app.NewApplication()
+	if err != nil {
+		panic(err)
+	}
+
+	http.HandleFunc("/health", HealthCheckHandler)
+	server := &http.Server{
+		Addr:         fmt.Sprintf(":%d", port),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+	app.Logger.Printf("Starting application... Server started on :%d\n", port)
+
+	err = server.ListenAndServe()
+	if err != nil {
+		app.Logger.Fatalf("Error starting server: %v", err)
+	}
+
+}
+
+func HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Status is available")
+}
