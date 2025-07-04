d074c1bcf22402974e9b98748b60baf94890867a
AGX <agxnh2020@gmail.com>
2025-07-04 03:53:42 +0300

    set the data layer and migration using goose

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,20 +1,33 @@
 package app
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/AGX18/goWorkoutAPI/internal/api"
+	"github.com/AGX18/goWorkoutAPI/internal/store"
+	"github.com/AGX18/goWorkoutAPI/migrations"
 )
 
 type Application struct {
 	Logger         *log.Logger
 	WorkoutHandler *api.WorkoutHandler
+	DB             *sql.DB
 }
 
 func NewApplication() (*Application, error) {
+	pgDB, err := store.Open()
+	if err != nil {
+		return nil, fmt.Errorf("app: open database %w", err)
+	}
+
+	if err := store.MigrateFS(pgDB, migrations.FS, "."); err != nil {
+		panic(err)
+	}
+
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
 
 	// our stores will go here
@@ -25,6 +38,7 @@ func NewApplication() (*Application, error) {
 	app := &Application{
 		Logger:         logger,
 		WorkoutHandler: workoutHandler,
+		DB:             pgDB,
 	}
 	return app, nil
 }
